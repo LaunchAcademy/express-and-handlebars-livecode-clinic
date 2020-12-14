772c7bdf4ce83c197ab9aed373f9f09c6bfd70c7
Stephen Fang <safang99@gmail.com>
2020-12-14 18:16:39 -0500

    removed finished code from main branch

--- src/routes/magicalCreaturesRouter.js
+++ src/routes/magicalCreaturesRouter.js
@@ -7,26 +7,4 @@ import creatures from "../creatures.js"
 
 const magicalCreaturesRouter = new express.Router()
 
-magicalCreaturesRouter.get("/", (req, res) => {
-  res.render("creatures/index", { creatures: creatures })
-})
-
-magicalCreaturesRouter.get("/:creatureName", (req, res) => {
-  const creatureName = req.params.creatureName
-
-  const creatureData = creatures.find(
-    (creature) => creature.name === creatureName
-  )
-
-  // Another valid way, but less ideal as it would iterate over the whole array
-  // let creatureData
-  // creatures.forEach((creature) => {
-  //   if (creature.name === creatureName) {
-  //     creatureData = creature
-  //   }
-  // })
-
-  res.render("creatures/show", { creature: creatureData })
-})
-
 export default magicalCreaturesRouter
