8b63ecf3441c19269c4e45528f3743dcdc8263fd
dovakeen118 <kegillis118@gmail.com>
2021-06-14 17:27:30 -0400

    finished code

--- src/routes/magicalCreaturesRouter.js
+++ src/routes/magicalCreaturesRouter.js
@@ -1,10 +1,42 @@
 import express from "express"
 
-const fields = ["title", "url", "description"]
+const fields = ['title', 'url', 'description']
 
 // `creatures` variable should return to you the creature objects that you need!
-import creatures from "../creatures.js"
+import creaturesData from '../creatures.js'
 
 const magicalCreaturesRouter = new express.Router()
+
+magicalCreaturesRouter.get("/", (req, res) => {
+  // console.log(creaturesData)
+  
+  res.render("magicalCreatures/index", { creatures: creaturesData })
+})
+
+magicalCreaturesRouter.get("/:creatureName", (req, res) => {
+  const creatureString = req.params.creatureName
+  // debugger
+
+  // const creature = creaturesData.find((creature) => {
+  //   return creature.name === creatureString
+  // })
+
+  const creature = creaturesData.find((creature) => {
+    if (creature.name === creatureString) {
+      return creature
+    }
+  })
+
+  // const creatureToDisplay
+  // creaturesData.forEach((creature) => {
+  //   if (creature.name === creatureString) {
+  //     creatureToDisplay = creature
+  //   }
+  // })
+
+  // console.log(creature)
+
+  res.render("magicalCreatures/show", { creature: creature })
+})
 
 export default magicalCreaturesRouter
--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -3,10 +3,12 @@ import magicalCreaturesRouter from "./magicalCreaturesRouter.js"
 
 const rootRouter = new express.Router()
 
+// localhost:3000/
 rootRouter.get('/', (req, res) => {
   res.render('index')
 })
 
+// localhost:3000/magical-creatures
 rootRouter.use("/magical-creatures", magicalCreaturesRouter)
 
 export default rootRouter
